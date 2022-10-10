fa1f2c2c4ef3c6b05b81c13f81010535254cb348
Jannatul Ferdous <jannatul.ferdous@stonybrook.edu>
2022-10-10 02:31:00 -0400

    new changes

--- File.py
+++ File.py
@@ -1,1 +1,31 @@
+import json
+import os
+import sys
+import time
+import csv
+import pandas as pd
+import crontab
 
+
+# get current working directory
+cwd = os.getcwd()
+
+# print cwd
+print(cwd)
+
+# create a new dictionary with dummy data
+health_data = pd.read_json('https://health.data.ny.gov/resource/9ma3-vsuk.json')
+healh_data
+
+# save data to local csv file
+covid_data.to_csv('Data/health_data.csv', index = None )
+
+# get the current time
+now = time.time()
+
+# save current time as a string
+nowStr = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
+
+# create a new file in the current working directory
+with open(cwd + '/testFile_' + nowStr + '.txt', 'w') as f:
+    f.write(str(data))
